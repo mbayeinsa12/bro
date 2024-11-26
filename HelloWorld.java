d41e2ed5878aab1a3a93084753fad4c4d9153919
mbayeinsa12 <insam621.com@gmail.com>
2024-11-26 22:05:28 +0100

    Mr Mbaye legui mom chaque jour nala envoyer deux sans projet

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+package rt;
+
+public class HelloWorld{
+
+    public static void main(String[]args){
+
+        /*
+         * Mr mbaye je prefere recommence java en 0
+         * di deff di guiss
+         * sans recopier le code de Mr mbaye
+         */
+        System.out.println("Hello Mbaye");
+    }
+}
---
+++ Somme.java
@@ -0,0 +1,17 @@
+package rt;
+
+public class Somme{
+     
+    public static void main(String[] args) {
+        
+        /*
+         * Voici la somme de deux entier 
+         * il faut ma dieul samay matouway ci affaire yii rekk
+         * billahi sinon moy lol
+         */
+        int nbre1 = 100;
+        int nbre2 = 50;
+        int somme = nbre1 + nbre2;
+        System.out.println("afficher la somme " +somme);
+    }
+}
